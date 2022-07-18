05fdfee4005224b7d5c8a262ab64aefc6f1dd26c
Hazem722 <hazemba684@gmail.com>
2022-07-18 08:44:03 -0700

    searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,17 +10,22 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
+	char *p;
 
-	for (i = 0; *s != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		j = 0;
+		while (accept[j] != '\0')
 		{
-			if (*s == accept[j])
+			if (accept[j] == s[i])
 			{
-				return (s);
+				p = &s[i];
+				return (p);
 			}
+			j++;
 		}
-		s++;
+		i++;
 	}
-	return (NULL);
+	return (0);
 }
