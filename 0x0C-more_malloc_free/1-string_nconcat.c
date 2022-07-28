eb3a28eb42c775a3165d8856e91745395c3eac70
Hazem722 <hazemba684@gmail.com>
2022-07-28 05:24:40 -0700

    a function that concatenates two strings.

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string one
+ * @s2: string to append n bytes from to string one
+ * @n: bytes of s2 to append to s1
+ * Return: a pointer to a newly allocated space in memory,
+ * which contains s1,followed by the first n bytes of s2,
+ * and null terminated (SUCCESS) and NULL (FAILURE)
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k, l;
+	char *fusion;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	i++;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	j++;
+	if (n >= j)
+	{
+		fusion = malloc(sizeof(char) * (i + j) + 1);
+	} else
+	{
+		fusion = malloc(sizeof(char) * (i + n) + 1);
+	}
+	if (fusion == 0)
+		return (NULL);
+	for (k = 0; k < i; k++)
+	{
+		fusion[k] = s1[k];
+	}
+	if (n >= j)
+	{
+		for (l = 0; l < j; l++)
+		{
+			fusion[i + l] = s2[l];
+		}
+		fusion[i + l] = '\0';
+	} else
+	{
+		for (l = 0; l < n; l++)
+		{
+			fusion[i + l] = s2[l];
+		}
+		fusion[i + l] = '\0';
+	}
+	return (fusion);
+}
